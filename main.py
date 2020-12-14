b4354b42ad3cc0345ce68e3e3a246ed750179650
skwlh544818 <69035874+skwlh544818@users.noreply.github.com>
2020-12-14 19:59:34 +0800

    oj1417

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import tensorflow as tf
-a=2
-b=1
-c=tf.add(a,b)
-print(c)
+from collections import defaultdict
+def choice():
+    n=int(input())
+    a=input().split()
+    if n<1 or n!=len(a) or len(a)%2==0:
+        return '-1'
+    m=defaultdict(int)
+    for i in a:
+        m[i]+=1
+    for i in a:
+        if m[i]%2==1:
+            return i
+print(choice())
